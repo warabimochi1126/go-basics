6d6191223e7c09c3ec9798555c657f2a8a14658f
warabimochi <satomi920003@yahoo.co.jp>
2025-09-07 03:53:00 +0900

    array・slice

--- main.go
+++ main.go
@@ -1,54 +1,86 @@
 package main
 
-import (
-	"fmt"
-	"unsafe"
-)
+import "fmt"
 
 func main() {
-	// uint16 ⇒ 2byteのデータ型
-	var ui1 uint16
-	// 変数の先頭メモリアドレスを取得する ⇒ &を付ける
-	fmt.Println("ui1 address", &ui1)
-	// メモリアドレスの先頭が2Byteズレている事からuint16が2Byteのデータ型であることが分かる
-	var ui2 uint16
-	fmt.Println("ui2 address", &ui2)
+	// 配列をやる
+	var a1 [3]int
+	fmt.Println(a1, len(a1), cap(a1))
 
-	// ポインタ変数は変数の先頭メモリアドレスを持っている
-	// 型を指定するのは、先頭アドレスから何byte分かを指定するため
-	var p1 *uint16
-	fmt.Println("p1 address", p1)
-	p1 = &ui1
-	fmt.Println("p1 address", p1)
-	fmt.Println("p1 size", unsafe.Sizeof(p1))
-	fmt.Println("p1 oneself address:", &p1)
-	// ポインタ変数が指すメモリアドレスの値を取得する(dereference)
-	fmt.Println("p1 pointer next value", *p1)
-	*p1 = 1
-	fmt.Println("ui1 value", ui1)
+	// 宣言時に代入
+	var a2 = [3]int{10, 20, 30}
+	fmt.Println(a2, len(a2), cap((a2)))
 
-	// ダブルポインタ ⇒ ポインタ変数が更にポインタを持つ状態
-	var pp1 **uint16 = &p1
-	fmt.Println("pp1 value", pp1)
-	fmt.Println("pp1 address", &pp1)
-	fmt.Println("pp1 size", unsafe.Sizeof(pp1))
+	// 代入する値の数を要素数とする
+	var a3 = [...]int{100, 200}
+	fmt.Println(a3, len(a3), cap(a3))
 
-	fmt.Println("pp1 one dereference", *pp1)
-	fmt.Println("pp1 two dereference", **pp1)
+	// := での宣言では代入が必須
+	a4 := [3]int{300, 400}
+	fmt.Println(a4, len(a4), cap(a4))
 
-	**pp1 = 10
-	fmt.Println("ui1 value", ui1)
+	// スライス ⇒ 可変長配列
+	var s1 []int
+	s2 := []int{}
+	fmt.Println("s1:", s1, len(s1), cap(s1))
+	fmt.Println("s2:", s2, len(s2), cap(s2))
 
-	// shadowing ⇒ 別のスコープで変数を再定義することで、外側の変数にアクセスできなくする手法
-	ok, result := true, "A"
-	fmt.Println("outer result pointer", &result)
-	if ok {
-		result = "B"
-		fmt.Println("innner result pointer", &result)
-		println(result)
-	} else {
-		result := "C"
-		println(result)
-	}
-	println(result)
+	// var := のどちらかによってslice
+	fmt.Println(s1 == nil)
+	fmt.Println(s2 == nil)
+
+	// appendで要素を追加
+	s1 = append(s1, 1, 2, 3)
+	fmt.Println("s1:", s1, len(s1), cap(s1))
+
+	// 引数に展開する
+	s3 := []int{4, 5, 6}
+	s1 = append(s1, s3...)
+	fmt.Println("s1:", s1, len(s1), cap(s1))
+
+	// makeで配列を作成する
+	s4 := make([]int, 0, 2)
+	fmt.Println("s4:", s4, len(s4), cap(s4))
+	s4 = append(s4, 1, 2, 3, 4)
+	fmt.Println("s4:", s4, len(s4), cap(s4))
+
+	s5 := make([]int, 4, 6)
+	fmt.Println("s5:", s5, len(s5), cap(s5))
+
+	// スライスから分割してスライスを作成する
+	s6 := s5[1:3]
+	s6[1] = 10
+	fmt.Println("s5:", s5, len(s5), cap(s5))
+	fmt.Println("s6:", s6, len(s6), cap(s6))
+
+	s6 = append(s6, 2)
+	fmt.Println("s5 after append:", s5, len(s5), cap(s5))
+	fmt.Println("s6 after append:", s6, len(s6), cap(s6))
+
+	fmt.Println("s5 length:", len(s5[1:3]))
+
+	// コピーの使い方
+	sc6 := make([]int, len(s5[1:3]))
+	fmt.Println("s5 copy:", s5, len(s5), cap(s5))
+	fmt.Println("sc6 copy before:", sc6, len(sc6), cap(sc6))
+
+	copy(sc6, s5[1:3])
+	fmt.Println("sc6 copy after:", sc6, len(sc6), cap(sc6))
+
+	sc6[1] = 12
+	fmt.Println("s5:", s5, len(s5), cap(s5))
+	fmt.Println("sc6:", sc6, len(sc6), cap(sc6))
+
+	s5 = make([]int, 4, 6)
+	fs6 := s5[1:3:3]
+	fmt.Println("s5:", s5, len(s5), cap(s5))
+	fmt.Println("fs6:", fs6, len(fs6), cap(fs6))
+	fs6[0] = 6
+	fs6[1] = 7
+	fs6 = append(fs6, 8)
+	fmt.Println("s5:", s5, len(s5), cap(s5))
+	fmt.Println("fs6:", fs6, len(fs6), cap(fs6))
+	s5[3] = 9
+	fmt.Println("s5:", s5, len(s5), cap(s5))
+	fmt.Println("fs6:", fs6, len(fs6), cap(fs6))
 }
