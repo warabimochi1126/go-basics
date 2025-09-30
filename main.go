ca71a7605224bd400129e8167517e11c42e2847a
warabimochi <satomi920003@yahoo.co.jp>
2025-10-01 02:45:05 +0900

    interface

--- main.go
+++ main.go
@@ -1,50 +1,43 @@
 package main
 
+import "fmt"
+
+// メソッド群を一覧に出来る ← interface
+type ageIncrementer interface {
+	ageUp() int
+}
+
+type human1 struct {
+	age int
+}
+type human2 struct {
+	age int
+}
+
+// 構造体にinterfaceと同名のメソッドを実装する
+func (human *human1) ageUp() int {
+	human.age += 1
+	return human.age
+}
+func (human *human2) ageUp() int {
+	human.age += 2
+	return human.age
+}
+
+func (human *human1) String() string {
+	return fmt.Sprint("String method")
+}
+
+func ageUp(human ageIncrementer) {
+	fmt.Println("human Age:", human.ageUp())
+}
+
 func main() {
-	// // コピーの使い方
-	// sc6 := make([]int, len(slice5[1:3]))
-	// fmt.Println("slice5 copy:", slice5, len(slice5), cap(slice5))
-	// fmt.Println("sc6 copy before:", sc6, len(sc6), cap(sc6))
+	human1 := &human1{1}
+	ageUp(human1) // human Age: 2
+	human2 := &human2{1}
+	ageUp(human2) // human Age: 3
 
-	// copy(sc6, slice5[1:3])
-	// fmt.Println("sc6 copy after:", sc6, len(sc6), cap(sc6))
-
-	// sc6[1] = 12
-	// fmt.Println("slice5:", slice5, len(slice5), cap(slice5))
-	// fmt.Println("sc6:", sc6, len(sc6), cap(sc6))
-
-	// slice5 = make([]int, 4, 6)
-	// fslice6 := slice5[1:3:3]
-	// fmt.Println("slice5:", slice5, len(slice5), cap(slice5))
-	// fmt.Println("fslice6:", fslice6, len(fslice6), cap(fslice6))
-	// fslice6[0] = 6
-	// fslice6[1] = 7
-	// fslice6 = append(fslice6, 8)
-	// fmt.Println("slice5:", slice5, len(slice5), cap(slice5))
-	// fmt.Println("fslice6:", fslice6, len(fslice6), cap(fslice6))
-	// slice5[3] = 9
-	// fmt.Println("slice5:", slice5, len(slice5), cap(slice5))
-	// fmt.Println("fslice6:", fslice6, len(fslice6), cap(fslice6))
-
-	// // 	map ⇒ 連想配列
-	// var m1 map[string]int
-	// m2 := map[string]int{}
-	// fmt.Println(m1, m1 == nil)
-	// fmt.Println(m2, m2 == nil)
-	// m2["A"] = 10
-	// m2["B"] = 20
-	// m2["C"] = 0
-	// fmt.Println(m2, len(m2), m2["A"])
-
-	// delete(m2, "A")
-	// fmt.Println(m2, len(m2), m2["A"])
-
-	// value, ok := m2["A"]
-	// fmt.Println(value, ok)
-	// value, ok = m2["C"]
-	// fmt.Println(value, ok)
-
-	// for key, value := range m2 {
-	// 	fmt.Println(key, value)
-	// }
+	fmt.Println(human1) // String method
+	fmt.Println(human2) // &{3}
 }
