40ed354502a591b70326ba29d9f4b14e8fba9cb1
warabimochi <satomi920003@yahoo.co.jp>
2025-10-09 22:15:50 +0900

    unit test

--- main.go
+++ main.go
@@ -1,57 +1,17 @@
 package main
 
-import (
-	"fmt"
-
-	"golang.org/x/exp/constraints"
-)
-
-// 型に~を付けると、その型を基底型とした独自の型も許容する
-// ex) この場合int型を基底型とした独自の型も許容する
-type customConstraints interface {
-	~int | int16 | float32 | float64 | string
-}
-
-// int型を基底型として独自の型を定義
-type NewInt int
-
-func add[T customConstraints](x, y T) T {
+func Add(x, y int) int {
 	return x + y
 }
-
-func min[T constraints.Ordered](x, y T) T {
-	if x < y {
-		return x
+func Divide(x, y int) float32 {
+	if y == 0 {
+		return 0
 	}
-	return y
-}
-func sumValues[K int | string, V constraints.Float | constraints.Integer](m map[K]V) V {
-	var sum V
-	for _, v := range m {
-		sum += v
-	}
-	return sum
+	return float32(x) / float32(y)
 }
 
 func main() {
-	fmt.Println(add(1, 2))
-	fmt.Println(add(1.1, 2.1))
-	fmt.Println(add("file", ".txt"))
-
-	var int1, int2 NewInt = 3, 4
-	fmt.Println(add(int1, int2))
-	fmt.Println(min(int1, int2))
-
-	m1 := map[string]uint{
-		"A": 1,
-		"B": 2,
-		"C": 3,
-	}
-	m2 := map[int]float32{
-		1: 1.23,
-		2: 4.56,
-		3: 7.89,
-	}
-	fmt.Println(sumValues(m1))
-	fmt.Println(sumValues(m2))
+	// x, y := 3, 5
+	// fmt.Println(Add(x, y))
+	// fmt.Println(Divide(x, y))
 }
